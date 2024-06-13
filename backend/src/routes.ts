26572b066565d39bb67df8e8f3239932c357a488
Caio <caio@localhost.localdomain>
2024-06-13 12:13:12 -0300

    orders functions and routes added

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -9,6 +9,11 @@ import { CreateCategoryController } from "./controllers/category/CreateCategoryC
 import { ListCategoryController } from "./controllers/category/ListCategoryController"
 import { CreateProductController } from "./controllers/product/CreateProductController"
 import { ListByCategoryController } from "./controllers/product/ListByCategoryController"
+import { CreateOrderController } from "./controllers/order/CreateOrderController"
+import { RemoveOrderController } from "./controllers/order/RemoveOrderController"
+import { AddItemController } from "./controllers/order/AddItemController"
+import { RemoveItemController } from "./controllers/order/RemoveItemController"
+import { SendOrderController } from "./controllers/order/SendOrderController"
 
 // middlewares
 import { isAuthenticated } from "./middlewares/isAuthenticated"
@@ -22,19 +27,22 @@ const upload = multer(uploadConfig.upload("./tmp"))
 
 // -- ROUTES USER --
 router.post('/users', new CreateUserController().handle)
-
 router.post('/sessions', new AuthUserController().handle)
-
 router.get('/me', isAuthenticated, new DetailUserController().handle)
 
 // -- ROUTES CATEGORY --
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
-
 router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
 // -- ROUTES PRODUCT -- 
 router.post('/product', isAuthenticated, upload.single("file"), new CreateProductController().handle)
-
 router.get('/category/product', isAuthenticated, new ListByCategoryController().handle)
+
+// -- ROUTES ORDER --
+router.post('/order', isAuthenticated, new CreateOrderController().handle)
+router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
+router.post('/order/add', isAuthenticated, new AddItemController().handle)
+router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
+router.put('/order/send', isAuthenticated, new SendOrderController().handle)
 
 export { router }
