e1ed65a8b9729a3ddb70a71eb61dccfa5754bac5
Caio <caio@localhost.localdomain>
2024-06-17 00:18:53 -0300

    mobile - login almost done, needs to persist login

---
+++ mobile/src/pages/Dashboard/index.tsx
@@ -0,0 +1,21 @@
+import React, {useContext} from 'react'
+import { View, Text, Button } from 'react-native'
+
+import { AuthContext } from '../../contexts/AuthContext'
+
+
+export default function Dashboard() {
+  const { signOut } = useContext(AuthContext)
+
+  return(
+    <View>
+      <Text>
+        Tela Dashboard
+      </Text>
+      <Button
+        title='Sair'
+        onPress={signOut}
+      />
+    </View>
+  )
+}
