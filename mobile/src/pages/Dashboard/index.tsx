18466b9f29c0dc1013fa6db3268f1c32e49ad8e5
Caio <caio@localhost.localdomain>
2024-06-17 15:27:48 -0300

    mobile - last update at product modal

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,21 +1,93 @@
-import React, {useContext} from 'react'
-import { View, Text, Button } from 'react-native'
+import React, {useState} from 'react'
+import { Text, SafeAreaView, TouchableOpacity, TextInput, StyleSheet } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 
-import { AuthContext } from '../../contexts/AuthContext'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import { StackParamsList } from '../../routes/app.routes'
 
+import { api } from '../../services/api'
 
 export default function Dashboard() {
-  const { signOut } = useContext(AuthContext)
+  const navigation = useNavigation<NativeStackNavigationProp<StackParamsList>>()
+
+  const [number, setNumber] = useState('')
+
+  async function openOrder(){
+    if(!number){
+      return
+    }
+
+    const response = await api.post('/order', {
+      table: Number(number)
+    })
+
+    navigation.navigate('Order', { number: response.data.table, orderId: response.data.id})
+
+    setNumber('')
+
+  }
 
   return(
-    <View>
-      <Text>
-        Tela Dashboard
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>
+        Novo Pedido
       </Text>
-      <Button
-        title='Sair'
-        onPress={signOut}
+      <TextInput
+        style={styles.input}
+        placeholder='Número da mesa'
+        placeholderTextColor='#f0f0f0'
+        keyboardType='numeric'
+        value={number}
+        onChangeText={setNumber}
       />
-    </View>
+      <TouchableOpacity 
+        style={styles.button}
+        onPress={openOrder}
+      >
+        <Text style={styles.buttonText}>
+          Abrir mesa
+        </Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 15,
+    backgroundColor: '#1d1d2e'
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginBottom: 24
+  },
+  input: {
+    width: '90%',
+    height: 60,
+    backgroundColor: '#101026',
+    borderRadius: 4,
+    paddingHorizontal: 8,
+    textAlign: 'center',
+    fontSize: 22,
+    color: '#fff'
+  },
+  button: {
+    width: '90%',
+    height: 40,
+    backgroundColor: '#3fffa3',
+    borderRadius: 4,
+    marginVertical: 12,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#101026',
+    fontWeight: 'bold'
+  }
+})
